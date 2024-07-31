b5ed84846ccc944d3414e65c0ea495c80ba33f6f
jeansdev1 <jeanv_lima@outlook.com>
2024-07-31 16:51:15 -0300

    implementacao da seguranca da aplicacao

--- src/usuario/controllers/usuario.controller.ts
+++ src/usuario/controllers/usuario.controller.ts
@@ -1,6 +1,7 @@
-import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put } from "@nestjs/common";
+import { Body, Controller, Get, HttpCode, HttpStatus, Param, ParseIntPipe, Post, Put, UseGuards } from "@nestjs/common";
 import { UsuarioService } from "../services/usuario.service";
 import { Usuario } from "../entities/usuario.module";
+import { JwtAuthGuard } from "../../auth/guard/jwt-auth.guard";
 
 
 @Controller("/usuarios")
@@ -8,12 +9,15 @@ export class UsuarioController{
 
     constructor(private readonly usuarioService: UsuarioService){ }
 
+    // so pode ser executado mediante validacao do token
+    @UseGuards(JwtAuthGuard)
     @Get('/all')
     @HttpCode(HttpStatus.OK)
     findAll(): Promise<Usuario[]>{
         return this.usuarioService.findAll();
     }
 
+    @UseGuards(JwtAuthGuard)
     @Get('/:id')
     @HttpCode(HttpStatus.OK)
     findById(@Param('id', ParseIntPipe) id: number): Promise<Usuario>{
@@ -26,6 +30,7 @@ export class UsuarioController{
         return this.usuarioService.create(usuario)
     }
 
+    @UseGuards(JwtAuthGuard)
     @Put('/atualizar')
     @HttpCode(HttpStatus.OK)
     async update(@Body() usuario: Usuario): Promise<Usuario>{
