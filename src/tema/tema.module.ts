7b3c5772d2de11f214ab463403d58cd5ebe19809
jeansdev1 <jeanv_lima@outlook.com>
2024-07-30 23:01:27 -0300

    bug git

--- src/tema/tema.module.ts
+++ src/tema/tema.module.ts
@@ -1,15 +1,8 @@
 import { Module } from "@nestjs/common";
 import { TypeOrmModule } from "@nestjs/typeorm";
 import { Tema } from "./entities/tema.entity";
-<<<<<<< HEAD
 import { TemaController } from "./controllers/tema.controller";
 import { TemaService } from "./services/tema.service";
-
-
-=======
-import { TemaService } from "./services/tema.service";
-import { TemaController } from "./controllers/tema.controller";
->>>>>>> af2ced332addb4dddb60666f941b8a4ff7b592fd
 
 @Module({
     imports: [TypeOrmModule.forFeature([Tema])],
@@ -17,8 +10,4 @@ import { TemaController } from "./controllers/tema.controller";
     controllers: [TemaController],
     exports: [TypeOrmModule]
 })
-<<<<<<< HEAD
 export class TemaModule {}
-=======
-export class TemaModule {}
->>>>>>> af2ced332addb4dddb60666f941b8a4ff7b592fd
